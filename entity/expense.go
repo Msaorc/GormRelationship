38a16c809858c9e13e40ac00dbac4964fba0949c
Marcos Augusto <msaorc@hotmail.com>
2023-03-27 11:54:05 -0300

    Review of relationships with gorm

--- entity/expense.go
+++ entity/expense.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Expense struct {
-	ID             entity.ID `gorm:"primaryKey"`
-	Description    string
-	Value          float64
-	Note           string
-	ExpenseLevelID entity.ID `gorm:"size:36"`
-	ExpenseLevel
+	ID              entity.ID `gorm:"primaryKey"`
+	Description     string
+	Value           float64
+	Note            string
+	ExpenseLevelID  entity.ID `gorm:"size:36"`
+	ExpenseLevel    ExpenseLevel
 	ExpenseOriginID entity.ID `gorm:"size:36"`
-	ExpenseOrigin
+	ExpenseOrigin   ExpenseOrigin
 	gorm.Model
 }
